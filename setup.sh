c4bd01076f0d7d787152ac3b9e4806f555491e4d
felipesms2 <fmbh.1989@gmail.com>
2025-02-26 19:36:18 -0300

    remove .git folder

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ sudo apt update && sudo apt install git -y
 FOLDER_NAME=$(tr -dc 'a-z' </dev/urandom | head -c 10)
 git clone https://github.com/felipesms2/docker-web.git "$FOLDER_NAME"
 sudo chmod -R 777 . 
+rm -rf "$FOLDER_NAME/.git"
 mv "$FOLDER_NAME"/* "$FOLDER_NAME"/.* . 2>/dev/null
 rm "$FOLDER_NAME" -rf
 mv -f ports.conf resources/config/
