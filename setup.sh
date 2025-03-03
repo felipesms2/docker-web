3fc3f325d465e57af35e28c23f5514aba05e468e
felipesms2 <fmbh.1989@gmail.com>
2025-03-03 15:17:03 -0300

    update setup sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,9 @@ mkdir -p resources/config
 sudo apt update && sudo apt install git -y
 FOLDER_NAME=$(tr -dc 'a-z' </dev/urandom | head -c 10)
 git clone https://github.com/felipesms2/docker-web.git "$FOLDER_NAME"
-sudo chmod -R 777 . &&  rm -rf .git
-cd "$FOLDER_NAME" 
+sudo chmod -R 777 && \
+cd "$FOLDER_NAME"  && \
+rm -rf .git && \
 mv "$FOLDER_NAME"/* "$FOLDER_NAME"/.* . 2>/dev/null
 rm "$FOLDER_NAME" -rf
 mv -f ports.conf resources/config/
